7007cc0febe0c9b550a7f437016e934cc5d27c25
Graceyard <tanhuishan@graduate.utm.my>
2025-02-08 15:58:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -162,4 +162,9 @@ df_prediction_proba = pd.DataFrame(prediction_proba)
 df_prediction_proba.columns = ['Rejected','Approved']
 df_prediction_proba.rename(columns={0:'Rejected',
                                  1:'Approved'})
-df_prediction_proba
+# df_prediction_proba
+
+# Display predicted outcome of loan status
+st.subheader('Predicted Outcome')
+loan_outcome = np.array(['Rejected', 'Approved'])
+st.success(str(loan_outcome[prediction][0]))
