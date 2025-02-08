4324795f71a3d87a12de632b3bd08974f7375df5
Graceyard <tanhuishan@graduate.utm.my>
2025-02-08 15:51:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -158,6 +158,7 @@ model.fit(X, y)
 prediction = model.predict(input_row_reshaped)
 prediction_proba = model.predict_proba(input_row_reshaped)
 
+df_prediction_proba = pd.DataFrame(prediction_proba)
+df_prediction_proba.columns = ['Rejected','Approved']
 prediction_proba.rename(columns={0:'Rejected',
                                  1:'Approved'})
-prediction_proba
