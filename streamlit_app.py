59adfd475f7539423f4a00a5e796067b7e170267
Graceyard <tanhuishan@graduate.utm.my>
2025-02-08 14:28:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -128,8 +128,6 @@ def target_encode(val):
     return target_mapper[val]
 
 y = y_raw.apply(target_encode)
-y
-y_raw
 
 with st.expander('Data Preparation'):
     st.write('**Encoded input loan (X)**')
