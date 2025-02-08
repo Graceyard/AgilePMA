d8e2963caec056ccc85db76fa6689bdaefea7f8a
Graceyard <tanhuishan@graduate.utm.my>
2025-02-08 15:05:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,12 +84,12 @@ with st.sidebar:
 
 # Financial & Property Information
     st.header("Financial Details")
+    property_area = st.radio("Property Area", ["Rural", "Semi-Urban", "Urban"])
     applicant_income = st.text_input("Applicant Income ($)")
     coapplicant_income = st.text_input("Co-applicant Income ($)")
     loan_amount = st.slider("Loan Amount (in thousands)", min_value=1, max_value=500, step=1, value=100)
     loan_amount_term = st.selectbox("Loan_Amount_Term", ["12", "36", "60", "84", "120", "180", "240", "300", "360", "480"])
     credit_history = st.radio("Credit History Meets Guidelines?", ["0", "1"])
-    property_area = st.radio("Property Area", ["Rural", "Semi-Urban", "Urban"])
 
 # Create DF for input features 
     data = {'Gender': gender,
