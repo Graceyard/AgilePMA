2ec846428b330d3b6f8886a386fe2e2dd4aecafe
Graceyard <tanhuishan@graduate.utm.my>
2025-02-08 15:19:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -141,7 +141,7 @@ with st.expander('Data Preparation'):
 
 # Ensure input_row alinged with the model input
 model_columns = X.columns
-input_row = pd.DataFrame(input_row_reshaped, columns=model_columns)
+input_row = pd.DataFrame(input_row, columns=model_columns)
 
 # Use RF to train the classification model (refer Colab)
 model = RandomForestClassifier(n_estimators=100, random_state=42)
