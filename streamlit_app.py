a2914b037910679b890c7bdba95e13d85cf5373d
Graceyard <tanhuishan@graduate.utm.my>
2025-02-09 21:31:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt 
 from sklearn.ensemble import RandomForestClassifier
 
-
-# Checkbox for agreement
-agree = st.checkbox("I confirm that I will not train any models or algorithms using this data.")
-
-# Only show content if the user agrees
-if agree:
-    st.write("Now you can access the data and insights here.")
-    # Add your data display logic here (tables, charts, etc.)
-
 # Streamlit Title
 st.title('Loan Eligibility Checker 🏠💰🤝')
 st.markdown("""
